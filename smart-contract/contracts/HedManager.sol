ac6070d73542732955b92164317a26220dfa4c63
bomanaps <mercy.boma35@yahoo.com>
2023-06-25 03:45:31 +0100

    smart contract updaet

--- smart-contract/contracts/HedManager.sol
+++ smart-contract/contracts/HedManager.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
@@ -29,28 +30,10 @@ contract HedManager {
         string name;
         string bio;
         string country;
-        string sdgCategory;
-        string image;
+        string avatar;
         string twitter;
-        string youtube;
-        string instagram;
-        uint256 amountRecieved;
-    }
-
-    struct Project {
-        uint256 projectId;
-        uint256 explorerId;
-        address payable owner;
-        string explorerName;
-        string cover;
-        string title;
-        Doners doners;
-        string country;
-        string community;
-        uint256 target;
-        string description;
-        string sdgCategory;
-        uint256 amountRecieved;
+        string github;
+        uint256 amountReceived; // Updated field name
     }
 
     struct Doners {
@@ -59,22 +42,37 @@ contract HedManager {
         uint256 amount;
     }
 
+    struct Project {
+        uint256 projectId;
+        uint256 explorerId;
+        address payable owner;
+        string explorerName;
+        string image;
+        string title;
+        Doners doners;
+        string github_link;
+        string demo;
+        string website;
+        uint256 target_amount;
+        string description;
+        string Category;
+        uint256 amountReceived; // Updated field name
+    }
+
     mapping(uint256 => Explorer) explorers;
     mapping(uint256 => Project) project;
     mapping(string => uint256) category;
     mapping(address => bool) public isRegistered;
     mapping(uint256 => mapping(uint256 => Doners)) public doners;
 
-    //create functions
-    function createEplorer(
+    // Create functions
+    function createExplorer(
         string memory _name,
         string memory _bio,
         string memory _country,
-        string memory _sdgCategory,
-        string memory _image,
+        string memory _avatar,
         string memory _twitter,
-        string memory _youtube,
-        string memory _instagram
+        string memory _github
     ) public {
         explorersCount.increment();
         Explorer storage _explorer = explorers[explorersCount.current()];
@@ -84,22 +82,20 @@ contract HedManager {
         _explorer.name = _name;
         _explorer.bio = _bio;
         _explorer.country = _country;
-        _explorer.sdgCategory = _sdgCategory;
-        _explorer.image = _image;
+        _explorer.avatar = _avatar;
         _explorer.twitter = _twitter;
-        _explorer.youtube = _youtube;
-        _explorer.instagram = _instagram;
+        _explorer.github = _github;
         isRegistered[msg.sender] = true;
         explorers[explorersCount.current()] = _explorer;
     }
 
     function createProject(
-        string memory _cover,
+        string memory _image,
         string memory _title,
-        string memory _country,
-        string memory _sdgCategory,
-        string memory _community,
-        uint256 _target,
+        string memory _github_link,
+        string memory _demo,
+        string memory _website,
+        uint256 _target_amount,
         string memory _description,
         uint256 _explorerId,
         string memory _explorerName
@@ -109,18 +105,18 @@ contract HedManager {
         _project.projectId = projectCount.current();
         _project.explorerId = _explorerId;
         _project.explorerName = _explorerName;
-        _project.cover = _cover;
+        _project.image = _image;
         _project.title = _title;
-        _project.country = _country;
-        _project.sdgCategory = _sdgCategory;
-        _project.community = _community;
-        _project.target = _target;
+        _project.github_link = _github_link;
+        _project.demo = _demo;
+        _project.website = _website;
+        _project.target_amount = _target_amount;
         _project.description = _description;
         _project.owner = payable(address(msg.sender));
         project[projectCount.current()] = _project;
     }
 
-    function addSponser(
+    function addSponsor(
         uint256 _id,
         uint256 _userId,
         string memory _categoryName
@@ -128,55 +124,47 @@ contract HedManager {
         require(_id > 0 && _id <= projectCount.current());
         Project storage _project = project[_id];
         Explorer storage _explorer = explorers[_userId];
-        category[_categoryName] = category[_categoryName] + msg.value;
+        category[_categoryName] = category[_categoryName].add(msg.value);
         address payable _owner = payable(address(_project.owner));
         _owner.transfer(msg.value);
-        _project.amountRecieved = _project.amountRecieved + msg.value;
-        _explorer.amountRecieved = _explorer.amountRecieved + msg.value;
+        _project.amountReceived = _project.amountReceived.add(msg.value);
+        _explorer.amountReceived = _explorer.amountReceived.add(msg.value);
         project[_id] = _project;
         explorers[_userId] = _explorer;
     }
 
-    //get functions
+    // Get functions
 
-    //get EthUsd
+    // Get ETH/USD price
     function getEthUsd() public view returns (uint256) {
         (, int256 price, , , ) = eth_usd_price_feed.latestRoundData();
         return price.toUint256();
     }
 
-    function getExplorer() public view returns (Explorer[] memory) {
+    function getExplorers() public view returns (Explorer[] memory) {
         uint256 itemCount = explorersCount.current();
-        uint256 currentIndex = 0;
-        Explorer[] memory _explorer = new Explorer[](itemCount);
+        Explorer[] memory _explorers = new Explorer[](itemCount);
         for (uint256 i = 0; i < itemCount; i++) {
             uint256 currentId = i + 1;
             Explorer storage currentItem = explorers[currentId];
-            _explorer[currentIndex] = currentItem;
-            currentIndex += 1;
+            _explorers[i] = currentItem;
         }
-        return _explorer;
+        return _explorers;
     }
 
-    function getProject() public view returns (Project[] memory) {
+    function getProjects() public view returns (Project[] memory) {
         uint256 itemCount = projectCount.current();
-        uint256 currentIndex = 0;
-        Project[] memory _project = new Project[](itemCount);
+        Project[] memory _projects = new Project[](itemCount);
         for (uint256 i = 0; i < itemCount; i++) {
             uint256 currentId = i + 1;
             Project storage currentItem = project[currentId];
-            _project[currentIndex] = currentItem;
-            currentIndex += 1;
+            _projects[i] = currentItem;
         }
-        return _project;
+        return _projects;
     }
 
     function isRegisteredFunc() public view returns (bool) {
-        if (isRegistered[msg.sender] == true) {
-            return true;
-        } else {
-            return false;
-        }
+        return isRegistered[msg.sender];
     }
 
     function getCategoryPrice(string memory _category)
